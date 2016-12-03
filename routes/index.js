28c8d58e2810fa9c767d49c95b7507ff42d8e398
Scott Heinrich <scottaheinrich@gmail.com>
2016-12-02 23:21:23 -0700

    updated index.ejs

--- routes/index.js
+++ routes/index.js
@@ -15,10 +15,10 @@ router.get('/email', function (req, res, next) {
   //setup email data with unicode symbols
   console.log(req.body.message)
   var mailOptions = {
-    from: 'scottaheinrich@gmail.com',
+    from: 'req.body.email',
     to: 'scottaheinrich@gmail.com',
     subject: "Hello",
-    email: 'scottaheinrich@gmail.com',
+    email: 'req.body.email',
     text: req.body.message
   };
   // send mail with defined transport object
